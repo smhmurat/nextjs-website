b484815094132c5bf038355ceb2d057f7a5db2d5
Semih Murat <smhmurat@gmail.com>
2022-03-24 09:27:27 +0300

    gtag.js eklendi.

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,21 @@
 import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer'
+import Script from 'next/script';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
       <Head>
         <title>Av. Beytullah MURAT | Murat Hukuk ve Danışmanlık</title>
         <meta name="google-site-verification" content="A-wE4tnc_Uzt6duGZyvDtUrBae3SVD1zZtiutF4vuBE" />
