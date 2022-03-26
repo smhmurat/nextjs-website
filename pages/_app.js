7cec28b53908e8c1c877e482bab3c47c0ac55565
Semih Murat <smhmurat@gmail.com>
2022-03-26 20:05:13 +0300

    _document.js eklendi.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { MainContext } from '../context/MainContext';
 import Layout from '../components/Layout'
 import '../styles/global.css';
@@ -6,6 +7,20 @@ import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   const [index, setIndex] = useState(0)
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   const values = {
     index,
     setIndex
